ba286a6912d7813f68eb6adb71ad1bc8a1add2c9
malikkurosaki <kurosakiblackangel@gmail.com>
2020-10-27 10:53:42 +0800

    ya ya

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "62b28793d3e4c66fd01191b2022bfd17",
-"/": "62b28793d3e4c66fd01191b2022bfd17",
-"main.dart.js": "b5f2653c875753badd6e4aab80848031",
+  "index.html": "1bec4b64e9b6bbfeb22a92df8fa1bc7c",
+"/": "1bec4b64e9b6bbfeb22a92df8fa1bc7c",
+"main.dart.js": "2f745065a402b66e282c3bf629f39364",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
